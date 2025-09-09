8084069d929a267e001cb2500e535f3078246f86
iAngelGuzman <agelgzman@outlook.com>
2025-09-09 08:15:29 -0600

    Inicio foro y perfil

--- paginas/inicio/inicio.js
+++ paginas/inicio/inicio.js
@@ -1,6 +1,6 @@
 // ---------------- Inicialización del mapa ----------------
 const map = L.map("map").setView([19.529825, -96.923362], 16);
-map.zoomControl.setPosition('topright');
+map.zoomControl.setPosition('bottomright');
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     maxZoom: 19,
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
